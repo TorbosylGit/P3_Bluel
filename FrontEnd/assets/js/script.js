44080869ab514f35070566e013a0a8d3e6dcc1de
TorbosylGit <gillesplazanet@hotmail.fr>
2024-06-10 14:21:04 +0200

    filtrer selon catégoeir

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -39,3 +39,46 @@ async function getFilters() {
         console.log("Erreur lors de la récupération des catégories :", error);
     }
 }
+// mettre à jour le bouton actif
+function updateActiveButton(activeButton) {
+    document.querySelectorAll('.button-style').forEach(button => {
+        button.classList.remove('active'); // retirer classe 'active'
+    });
+    activeButton.classList.add('active'); // ajouter classe 'active'
+}
+
+// récupérer/remplir liste des photos selon la catégorie
+async function getWorks(categoryId = '0') {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+        const data = await response.json();
+        const gallery = document.getElementsByClassName("gallery")[0];
+        gallery.innerHTML = ""; // vider container
+
+        // filtrer les données selon la catégorie
+        const filterData = data.filter(work => categoryId === '0' || work.categoryId.toString() === categoryId);
+
+        // créer et ajouter éléments pour chaque photo
+        filterData.forEach(work => {
+            const figure = document.createElement("figure");
+            figure.setAttribute("data-id", work.id);
+
+            const img = document.createElement("img");
+            img.setAttribute("src", work.imageUrl);
+            img.setAttribute("alt", work.title);
+
+            const figcaption = document.createElement("figcaption");
+            figcaption.innerText = work.title;
+
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// appeler les fonctions initiales
+getWorks();
+getFilters();
